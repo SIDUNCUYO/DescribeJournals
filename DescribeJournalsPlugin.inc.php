198e83a1d3483ee711ca592466ae13befd7eb9a3
Horacio Degiorgi <horaciod@gmail.com>
2023-08-30 08:49:08 -0300

    prefix, docs and copyright

--- DescribeJournalsPlugin.inc.php
+++ DescribeJournalsPlugin.inc.php
@@ -2,17 +2,19 @@
 /**
  * @file DescribeJournalsPlugin.inc.php
  *
- * Copyright (c) 2017-2021 Simon Fraser University
- * Copyright (c) 2017-2021 John Willinsky
+ * Copyright (c) 2023 SID - UNCuyo  
+ * Copyright (c) 2023 Horacio Degiorgi
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class DescribeJournalsPlugin
- * @brief Plugin class for the DescribeJournals plugin.
+ * @brief Plugin class for the DescribeJournals plugin. All the meta is saved as dj_* : dj_building, 
+ * dj_disciplines, dj_keywords,dj_indexed_in,dj_url_old_archives,dj_doi_asigned
  */
 import('lib.pkp.classes.plugins.GenericPlugin');
 class DescribeJournalsPlugin extends GenericPlugin {
 
-	var $optionsindexed_in = [	
+
+	var $optionsdj_indexed_in = [	
 		'Biblat',
 		'CLASE',
 		'Nucleo Básico',
@@ -47,50 +49,49 @@ public function getdescription() {
   }
 
   /**
-   * Extend the context entity's schema with an building property
+   * Extend the context entity's schema with an dj_building property
    */
   public function addToSchema($hookName, $args) {
 		$schema = $args[0];
-		$schema->properties->building = (object) [
+		$schema->properties->dj_building = (object) [
 			'type' => 'string',
 			'apiSummary' => true,
 			'multilingual' => false,
 			'validation' => ['nullable']
 		];
-		$schema->properties->doi_asigned = (object) [
+		$schema->properties->dj_doi_asigned = (object) [
 			'type' => 'string',
 			'apiSummary' => true,
 			'multilingual' => false,
 			'validation' => ['nullable']
 		];
-		$schema->properties->url_old_archives = (object) [
+		$schema->properties->dj_url_old_archives = (object) [
 			'type' => 'string',
 			'apiSummary' => true,
 			'multilingual' => false,
 			'validation' => ['nullable']
 		];
-		$schema->properties->disciplines = (object) [
+		$schema->properties->dj_keywords = (object) [
 			'type' => 'string',
 			'apiSummary' => true,
 			'multilingual' => false,
 			'validation' => ['nullable']
 		];
 
-    $schema->properties->indexed_in = (object) [
+    $schema->properties->dj_indexed_in = (object) [
         'type' => 'array',
-        //'apiSummary' => true,
         'multilingual' => false,
         'validation' => ['nullable'],
 		'items' => (object) [
 			'type' => 'string'
 			]
     ];
-//dump($schema->properties->indexed_in) ;
+
 		return false;
   }
 
   /**
-   * Extend the masthead form to add an building input field
+   * Extend the masthead form to add an dj_building input field
    * in the journal/press settings
    */
 	public function addtoForm($hookName, $form) {
@@ -110,54 +111,54 @@ public function getdescription() {
 		'label' => 'Información SID',
 	]) ; 
     // Add a field to the form
-		$form->addField(new \PKP\components\forms\FieldText('building', [
+		$form->addField(new \PKP\components\forms\FieldText('dj_building', [
 			'label' => 'Dependencia',
 			'groupId' => 'sid',
 			'isRequired'=>true,
-			'value' => $context->getData('building'),
+			'value' => $context->getData('dj_building'),
 			'description'=>'se utiliza para colocar la facultad a la que pertenece, colocar nombre completo. Ej: Facultad de Odontología'
 		]));
-		$form->addField(new \PKP\components\forms\FieldText('disciplines', [
+		$form->addField(new \PKP\components\forms\FieldText('dj_keywords', [
 			'label' => 'Disciplinas',
 			'groupId' => 'sid',//'publishing',
 			'isRequired'=>true,
-			'value' => $context->getData('disciplines'),
+			'value' => $context->getData('dj_keywords'),
 			'description'=>'Separar por comas'
 		]));
 		
 		
 	 //
 	 $values=[] ; 
-foreach($this->optionsindexed_in as $k => $v) {
+foreach($this->optionsdj_indexed_in as $k => $v) {
     $values[] = ['value' => str_replace(' ','_',strtolower($v)) , 'label' => $v ,'name' => $v] ;
 }
 
-		$form->addField(new \PKP\components\forms\FieldOptions('indexed_in', [
+		$form->addField(new \PKP\components\forms\FieldOptions('dj_indexed_in', [
 			'label' => 'Indexado en' ,
 			'groupId' => 'sid',//'publishing',
 			'type'=>'checkbox' ,
 			'isMultilingual' => false,
 			'options' => $values , 
-			'value' => $context->getData('indexed_in') ? $context->getData('indexed_in') : [],
+			'value' => $context->getData('dj_indexed_in') ? $context->getData('dj_indexed_in') : [],
 			]
 		)
 	);	
 		
-	$form->addField(new \PKP\components\forms\FieldOptions('doi_asigned', [
+	$form->addField(new \PKP\components\forms\FieldOptions('dj_doi_asigned', [
 		'label' => 'Registra DOIs',
 		'groupId' => 'sid',
 		'type'=>'radio', 
 		'isRequired'=>true,
 		'options'=>[['value'=>'false','label'=>'No'] , ['value'=>'true','label'=>'Si'] ]  , 
-		'value' => $context->getData('doi_asigned'),
+		'value' => $context->getData('dj_doi_asigned'),
 		'description'=>'¿Se le asignan DOIs a los artículos seleccionados ? '
 	]));
-	$form->addField(new \PKP\components\forms\FieldText('url_old_archives', [
+	$form->addField(new \PKP\components\forms\FieldText('dj_url_old_archives', [
 		'label' => 'URL histórico de artículos',
 		'groupId' => 'sid',
 		'inputType'=>'URL' , 
 		'isRequired'=>false,
-		'value' => $context->getData('url_old_archives'),
+		'value' => $context->getData('dj_url_old_archives'),
 		'description'=>'url completa para ver el archivo de los artículos anteriores a la migración '
 	]));
 	
